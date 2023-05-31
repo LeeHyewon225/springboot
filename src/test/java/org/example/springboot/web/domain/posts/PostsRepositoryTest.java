85a338f0fef77faae4000957433de27fe18bb2a6
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-05-31 23:45:21 +0900

    Spring Data JPA testcode

---
+++ src/test/java/org/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package org.example.springboot.web.domain.posts;
+
+import org.example.springboot.domain.posts.Posts;
+import org.example.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest  //H2 데이터베이스 자동 실행
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title).content(content).author("jojoldu@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
